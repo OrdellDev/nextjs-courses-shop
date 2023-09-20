e0269f9f647cb62c1f4f74d4bf750dc64a3cb240
OrdellDev <facundotanovichdev@gmail.com>
2023-09-20 18:15:44 -0300

    feat: :zap: Dinamyc Layout finish

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,8 @@
 import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
 
-const DashboardLayout = ({children}: {children: React.ReactNode}) => {
-    return ( 
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
         <div className="h-full">
             <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
                 <Navbar />
@@ -10,11 +10,11 @@ const DashboardLayout = ({children}: {children: React.ReactNode}) => {
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
-            <main className="md:pl-56 h-full">
+            <main className="md:pl-56 pt-[80px] h-full">
                 {children}
             </main>
         </div>
-     );
+    );
 }
- 
+
 export default DashboardLayout;
